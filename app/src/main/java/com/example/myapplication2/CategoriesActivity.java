1e0d90283341ebbb6cfa6157f212ef543090e4d8
juser0719 <sinsa0719@naver.com>
2019-11-26 12:35:05 +0900

    jjj

---
+++ app/src/main/java/com/example/myapplication2/CategoriesActivity.java
@@ -0,0 +1,20 @@
+package com.example.myapplication2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CategoriesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_categories);
+
+        Intent intent = getIntent();
+
+    }
+}
---
+++ app/src/main/java/com/example/myapplication2/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.myapplication2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.time.Instant;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView ImageView_add;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView_add =  findViewById(R.id.ImageView_add);
+        ImageView_add.setClickable(true);
+        ImageView_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this , CategoriesActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
